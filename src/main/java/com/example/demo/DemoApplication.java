cc042df37a3740db48327fc59a1a6b97424ad143
uki <wyspianski.lukasz@gmail.com>
2021-10-25 12:50:31 +0200

    JIRA-bugfix-007
    - Change the endpoint's url
    - Change the mesg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,9 +13,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello-spring")
     String greet() {
-        return "Hello, World!";
+        return "Hello, Spring";
     }
 
 }
